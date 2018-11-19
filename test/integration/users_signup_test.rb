5cb10123f70d9ac164dd8e9c8d29d6230e2793df
masaya-ishioka <penpenmono31@gmail.com>
2018-11-19 11:48:59 +0000

    user ssl and puma webserver in production

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -12,9 +12,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
 		end
 		follow_redirect!
 		assert_template "users/show"
-		assert_select "div#<CSS id for error explanation>"
-		assert_select "div.<CSS class for field with error>"
-		assert_not flash.FILL_IN
+		assert_not flash.empty?
 	end
 
 end
